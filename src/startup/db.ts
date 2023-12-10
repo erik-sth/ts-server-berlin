fb454e645045915f469ba06932ce6edf8bfd50a5
Erik <erik.p.schmitt@gmail.com>
2023-12-10 11:34:39 +0100

    Updated to use env var.

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -2,7 +2,17 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.db);
+        const mongoURI = process.env.MONGODB_URI;
+
+        if (!mongoURI) {
+            throw new Error(
+                'MongoDB connection string not found in environment variables.'
+            );
+        }
+
+        await mongoose.connect(mongoURI);
+
+        console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
         throw error;
